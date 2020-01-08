edb143413d9e9c5ce100ccfd2acf8c90e7e17e50
Sylvain Tran <45033804+peelingSun@users.noreply.github.com>
2020-01-08 15:26:54 -0500

    Activity 1: Replaced all hard-coded values into constants.

--- activities/activity 1/js/script.js
+++ activities/activity 1/js/script.js
@@ -1,18 +1,40 @@
 "use strict";
 
 /*****************
-
-Title of Project
-Author Name
-
-This is a template. You must fill in the title,
-author, and this description to match your project!
-
+Circle Eater
+Pippin Barr
+A simple game in which the player controls a shrinking circle with their mouse and tries
+to overlap another circle (food) in order to grow bigger.
 ******************/
+
+// Constants defining key quantities
+const PLAYER_SIZE_GAIN = 50;
+const PLAYER_SIZE_LOSS = 1;
+const YELLOW_GRASS_COLOR = '#cccc55';
+const FADED_BLUE = '#55cccc';
+const MINIMUM_SIZE = 0;
+
+// Player is an object defined by its properties
+let player = {
+  x: 0,
+  y: 0,
+  maxSize: 64,
+  size: 64,
+  active: true,
+  color: YELLOW_GRASS_COLOR
+}
+
+// Food is an object defined by its properties
+let food = {
+  x: 0,
+  y: 0,
+  size: 64,
+  color: FADED_BLUE
+}
 
 // preload()
 //
-// Description of preload
+// Not needed
 
 function preload() {
 
@@ -21,17 +43,93 @@ function preload() {
 
 // setup()
 //
-// Description of setup
+// Create the canvas, position the food, remove the cursor
 
 function setup() {
-
+  createCanvas(windowWidth, windowHeight);
+  positionFood();
+  noCursor();
 }
 
 
 // draw()
 //
-// Description of draw()
+// Move the player, check for collisions, display player and food
 
 function draw() {
+  // Make sure the player is still alive - if not, we don't run
+  // the rest of the draw loop
+  if (!player.active) {
+    // By using "return" the draw() function exits immediately
+    return;
+  }
 
+  // Otherwise we handle the game
+  background(0);
+  updatePlayer();
+  checkCollision();
+  displayPlayer();
+  displayFood();
+}
+
+// updatePlayer()
+//
+// Set the position to the mouse location
+// Shrink the player
+// Set it to inactive if it reaches a size of zero
+function updatePlayer() {
+  player.x = mouseX;
+  player.y = mouseY;
+  // Shrink the player and use constrain() to keep it to reasonable bounds
+  player.size = constrain(player.size - PLAYER_SIZE_LOSS, MINIMUM_SIZE, player.maxSize);
+  if (player.size === MINIMUM_SIZE) {
+    player.active = false;
+  }
+}
+
+// checkCollision()
+//
+// Calculate distance of player to food
+// Check if the distance is small enough to be an overlap of the two circles
+// If so, grow the player and reposition the food
+function checkCollision() {
+  let d = dist(player.x, player.y, food.x, food.y);
+  if (d < player.size / 2 + food.size / 2) {
+    player.size = constrain(player.size + PLAYER_SIZE_GAIN, MINIMUM_SIZE, player.maxSize);
+    positionFood();
+  }
+}
+
+// displayPlayer()
+//
+// Draw the player in its current position, using its size and color
+// Use push() and pop() around it all to avoid affecting the rest of the program
+// with drawing commands
+function displayPlayer() {
+  push();
+  noStroke();
+  fill(player.color);
+  ellipse(player.x, player.y, player.size);
+  pop();
+}
+
+// displayFood()
+//
+// Draw the food in its current position, using its size and color
+// Use push() and pop() around it all to avoid affecting the rest of the program
+// with drawing commands
+function displayFood() {
+  push();
+  noStroke();
+  fill(food.color);
+  ellipse(food.x, food.y, food.size);
+  pop();
+}
+
+// positionFood()
+//
+// Set the food's position properties to random numbers within the canvas dimensions
+function positionFood() {
+  food.x = random(0, width);
+  food.y = random(0, height);
 }

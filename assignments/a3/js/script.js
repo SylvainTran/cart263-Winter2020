f820a419ba3febab0587f0a2d065c2ea553485f4
SylvainTran <sylvain.serey.tran@protonmail.com>
2020-02-18 21:07:46 -0500

    a3: commented the assignment out and placed command objects in the annyang inits. Works most of the time though. Added basic css

--- assignments/a3/js/script.js
+++ assignments/a3/js/script.js
@@ -2,14 +2,13 @@
 
 /********************************************************************
 
-Title of Project
-Author Name
+Assignment 3 - Slamina Special
+Sylvain Tran
 
-This is a template. Fill in the title, author, and this description
-to match your project! Write JavaScript to do amazing things below!
+Sometimes it works, sometimes it doesn't. This game is a beautiful metaphor
+of life and love.
 
 *********************************************************************/
-
 $(document).ready(setup);
 let animals =
     [
@@ -154,35 +153,53 @@ let answers = [];
 const NUM_OPTIONS = 3;
 let backwardsText = "";
 
+// setup()
+//
+// setups annyang and inits the commands needed. Starts listening. Starts a new round
+// and also inits the score display
 function setup() {
   // Initialise annyang with no commands (because we just want to listen to whatever it hears)
   if(annyang)
   {
-    annyang.init({});
-    //annyang.addCallback('result', handleSpeech);
-    // Add our commands to annyang
+    // inits the commands
+    annyang.init(commands, true);
+    annyang.init(playerGuess, false);
+
+    // Add our commands to annyang (separated for clarity)
     annyang.addCommands(commands);
     annyang.addCommands(playerGuess);
 
-    // Start listening. You can call this here, or attach this call to an event, button, etc.
+    // Start listening
     annyang.start();
   }
-
   newRound();
   updateScore();
 }
 
+// removeDivs()
+//
+// Removes the guess divs. Called when setting a new round
+function removeDivs() {
+  $('.guess').remove();  
+}
+
+// newRound()
+//
+// Start a new round by clearing existing answer divs, creating new answer divs and a correct one.
 function newRound() {
+  removeDivs();
   updateScore();
   answers = [];
   let randomCorrect = Math.floor(Math.random() * answers.length);
 
-  for(let i = 0; i < NUM_OPTIONS; i++) {
+  for(let i = 0; i < NUM_OPTIONS; i++) 
+  {
     let randomIndex = Math.floor(Math.random() * animals.length);
     let randomAnimal = animals[randomIndex];
     let thisButton = addButton(randomAnimal);
     answers.push(randomAnimal);
 
+    // Checks if this div is gonna be the correct answer, if so add a class for correctAnimal for easy retrieval
     if(i === randomCorrect)
     {
       correctAnimal = answers[i];
@@ -193,27 +210,43 @@ function newRound() {
   sayBackwards(correctAnimal);
 }
 
+// options()
+//
+// Options for rate and pitch be random for some reason
 let options = {
   "rate": Math.random(),
   "pitch": Math.random()
 }
 
+// handleGuess()
+//
+// Handle the guesses via clicking -- if it's the correct one, remove the guesses
 function handleGuess() {
   if($(this).text() === correctAnimal) {
     $('.guess').remove();
+    consecutiveScore++;
+    updateScore();
     setTimeout(newRound, 2000);
   }
-  else {
+  else 
+  {
+    consecutiveScore = 0;
     $(this).effect('shake');
     responsiveVoice.speak(backwardsText, "UK English Male", options);
   }
 }
 
+// sayBackwards()
+//
+// Say the provided text arg backwards
 function sayBackwards(text) {
   backwardsText = text.split('').reverse().join('');
   responsiveVoice.speak(backwardsText, "UK English Male", options);
 }
 
+// addButton()
+//
+// Creates and appends a button with the guess class and a click handler, and returns its reference for later use
 function addButton(label) {
   let $div = $('<div></div>');
   $div.addClass("guess");
@@ -224,39 +257,53 @@ function addButton(label) {
   return $div;
 }
 
+// shakeCorrectDiv()
+//
+// Shake the correct div (the one with the correctAnimal class)
 function shakeCorrectDiv() {
-  $('.correctAnimal').shake();
+  $('.correctAnimal').effect("shake");
 }
 
+// Annyang commands and the number of times having answered correctly
 let vocalGuess;
-let consecutiveScore;
+let consecutiveScore = 0;
 
+// These actually worked (I swear) but it's unreliable and one needs
+// to speak like something is stuck up its arse
+// (and also with perfect slow and articulated pronunciation)
 let commands = {
   'I give up': function() {
-    alert("ok");
     consecutiveScore = 0;
     shakeCorrectDiv();
-    newRound();
+    setTimeout(newRound, 1000);
   },
   'Say it again': () => {
     responsiveVoice.speak(backwardsText, "UK English Male", options);
   },
 }
 
+// Checks the player's guess
+// Increases score if gotten correctly
+// Calls a new round after updating the score. If failed, reset score and start a new round
 let playerGuess = {
-  "I think it is *vocalGuess": function() {
+  'I think it is *vocalGuess': function(vocalGuess) {
     if(vocalGuess === correctAnimal) {
+      alert("Correct");
       consecutiveScore++;
+      updateScore();
       newRound();
     }
     else
     {
       consecutiveScore = 0;
+      updateScore();
     }
   }
 }
-
+// updateScore()
+//
+// Updates the score display
 function updateScore()
 {
-  $('score').text("Score: " + consecutiveScore);
+  $('#score').text("Score: " + consecutiveScore);
 }

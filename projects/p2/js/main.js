bdbc3f7e38ca4bca417a7ace24668ab3d428e1a2
SylvainTran <sylvain.serey.tran@protonmail.com>
2020-03-01 12:06:15 -0500

    P2: Refactored out annyang commands into VoiceCommands.js

--- projects/p2/js/main.js
+++ projects/p2/js/main.js
@@ -162,29 +162,5 @@ function checkMovement()
     }
 }
 
-let commands = {
-    'Start working': function() {
-        // if in a good mood or state, will obey
-        responsiveVoice.speak("Cleaning up the floor, sir. New Content Uploaded.", "UK English Female", options);
-        createNewYoutubeContent();
-        rotateMe();
-        // if not, disobey
-        //responsiveVoice.speak("Nah, I won't do it.", "UK English Female", options);        
-    },
-    'Stop working': () => {
-        responsiveVoice.speak("There wasn't much to do anyway.", "UK English Female", options);
-    },
-    'Take a stroll': () => {
-        responsiveVoice.speak("Everyone deserves a good stroll", "UK English Female", options);        
-    }
-}
-
-// options()
-//
-// Options for rate and pitch be random for some reason
-let options = {
-    "rate": Math.random(),
-    "pitch": Math.random()
-}
   
   

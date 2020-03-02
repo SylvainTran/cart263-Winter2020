21e1036384257e7bf6a10898829e1cd6bdd5afe1
SylvainTran <sylvain.serey.tran@protonmail.com>
2020-03-02 01:01:02 -0500

    P2: Added pixel art tilemaps

---
+++ projects/p2/assets/tilemaps/cuteRPG_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.0" name="cuteRPG_tileset" tilewidth="32" tileheight="32" tilecount="45" columns="9">
+ <image source="../../../../../../Desktop/Cart 263 - P2/Tiles.png" width="288" height="176"/>
+</tileset>
---
+++ projects/p2/assets/tilemaps/things_0.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.0" name="things_0" tilewidth="16" tileheight="16" tilecount="96" columns="12">
+ <image source="../../../../../../Desktop/Cart 263 - P2/Tiles/things_0.png" width="192" height="128"/>
+</tileset>

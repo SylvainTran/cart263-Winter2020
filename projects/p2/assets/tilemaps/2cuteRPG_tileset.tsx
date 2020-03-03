2954eb4d1e894ddfb189a19c257cab8be20a888c
SylvainTran <sylvain.serey.tran@protonmail.com>
2020-03-03 01:00:09 -0500

    P2: Added working world map using Tiled with collisions and spawn point

--- projects/p2/assets/tilemaps/2cuteRPG_tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.0" name="Tiles" tilewidth="16" tileheight="16" tilecount="198" columns="18">
- <image source="../../../../../../Desktop/Cart 263 - P2/Tiles.png" width="288" height="176"/>
-</tileset>
--- projects/p2/assets/tilemaps/BaseChip_pipo_tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.0" name="[Base]BaseChip_pipo" tilewidth="32" tileheight="32" tilecount="1064" columns="8">
- <image source="../../../../../../Desktop/Cart 263 - P2/[Base]BaseChip_pipo.png" width="256" height="4256"/>
-</tileset>

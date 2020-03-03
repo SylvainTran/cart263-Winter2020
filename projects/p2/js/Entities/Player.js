036b68ea4041627c55c3e61841b77097c3cedd55
SylvainTran <sylvain.serey.tran@protonmail.com>
2020-03-03 02:02:41 -0500

    P2: Reduced player's speed (not really used for now), and added code reference attribution for the tilemap stuff

--- projects/p2/js/Entities/Player.js
+++ projects/p2/js/Entities/Player.js
@@ -3,7 +3,7 @@ class Person extends Phaser.Physics.Arcade.Sprite {
     constructor(scene, x, y, avatarKey) {
         super(scene, x, y, avatarKey);
         scene.physics.world.enableBody(this);
-        this.speed = 50;
+        this.speed = 10;
         this.x = x;
         this.y = y;
     }
--- projects/p2/js/Scenes/YoutubeLounge.js
+++ projects/p2/js/Scenes/YoutubeLounge.js
@@ -10,12 +10,38 @@ class YoutubeLounge extends Phaser.Scene {
     }
     
     preload () 
-    {
-
+    {       
+        this.load.image("tilesA", "./assets/tilesets/tilesetA.png");
+        this.load.tilemapTiledJSON("map", "./assets/tilemaps/world-of-youtube.json");
     }
 
     create ()
     {
+        // Tilemap setup
+        // Followed tutorial from https://medium.com/@michaelwesthadley/modular-game-worlds-in-phaser-3-tilemaps-1-958fc7e6bbd6
+        const map = this.make.tilemap({ key: "map" });
+        const tileset = map.addTilesetImage("tilesetA", "tilesA");
+        const belowLayer = map.createStaticLayer("Below", tileset, 0, 0);
+        const worldLayer = map.createStaticLayer("World", tileset, 0, 0);
+        const aboveLayer = map.createStaticLayer("Above", tileset, 0, 0);
+        aboveLayer.setCollisionByProperty({ collides: true });
+
+        // Collision debug
+        const debugGraphics = this.add.graphics().setAlpha(0.75);
+        aboveLayer.renderDebug(debugGraphics, {
+          tileColor: null, // Color of non-colliding tiles
+          collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
+          faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
+        });
+        
+        // Player collision with tiles with collide true
+        // Spawn at the spawn point setup in Tiled
+        const spawnPoint = map.findObject("GameObjects", obj => obj.name === "Spawn Point");
+        this.youtubePimpPlayer = new YoutubePimpPlayer(this, spawnPoint.x, spawnPoint.y, "automata");
+        this.youtubePimpPlayer.setCollideWorldBounds(true);
+        this.physics.add.collider(this.youtubePimpPlayer, aboveLayer);
+
+        // Test NPC
         this.testAutomata = new Automata({scene:this, x: automataConfig.x, y: automataConfig.y});
         this.testAutomata.speak();
         console.log("In youtube lounge");
@@ -26,6 +52,7 @@ class YoutubeLounge extends Phaser.Scene {
 
     update(time, delta) 
     {
+        this.youtubePimpPlayer.PlayerFSM.step();
         this.testAutomata.AutomataFSM.step();
     }
 }

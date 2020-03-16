4f3a9dd941f9fec44c5cee77b926087a92eb889b
SylvainTran <sylvain.serey.tran@protonmail.com>
2020-03-16 17:44:02 -0400

    p3: Commented out the insetbox part, studied some more freebootcamp

--- projects/p3/js/Scenes/BootScene.js
+++ projects/p3/js/Scenes/BootScene.js
@@ -13,24 +13,34 @@ class BootScene extends Phaser.Scene {
   
     create() {
       // Append the phaser canvas in the flex box
-      let pocketDiv = document.createElement("div");
+      let pocketDiv = document.createElement('div');
       $('.pocket__game').append($(pocketDiv));
       $(pocketDiv).append($(this.game.canvas)); // Just this instance's canvas
-      $(this.game.canvas).draggable({axis: "x"}); // Restrain to x-axis (also the div flex colum direction does this somehow)
+      $(this.game.canvas).draggable({axis: 'x'}); // Restrain to x-axis (also the div flex colum direction does this somehow)
+      this.game.canvas.style.zIndex = 1;
       // Randomize offset positions relative to parent pocket__game div
       console.log($('.main__game').width());
       let $parentCanvasWidth = $('.main__game').width();
       let $parentCanvasHeight = $('.main__game').height();
 
+      // Create the inset box at the middle to sequence divs
+      // let insetBox = document.createElement('div');
+      // insetBox.style.position = 'fixed';
+      // insetBox.style.top = window.innerHeight + 'px';
+      // insetBox.style.left = window.innerWidth/2 + 'px';
+      // insetBox.style.width = '160px';
+      // insetBox.style.height = '160px';
+      // insetBox.style.backgroundColor = 'cyan';
+      // insetBox.style.zIndex = 0;
+      // $('.pocket__game').append(insetBox);
+
       $('canvas').each( (index, element) => {
-        //console.log(element.offsetTop);
-        //console.log(element.offsetLeft);
         let newTopPos = Math.floor(Math.random() * $parentCanvasHeight);
         let newLeftPos = Math.floor(Math.random() * $parentCanvasWidth);
-        console.log("New top pos: " + newTopPos);
-        console.log("New left pos: " + newLeftPos);
+        // console.log("New top pos: " + newTopPos);
+        // console.log("New left pos: " + newLeftPos);
         // for each element canvas, randomly place it within the bounds of the main__game's width and height bounds
-        console.log("Old top: " + element.style.top);
+        // console.log("Old top: " + element.style.top);
         element.style.position = "sticky";
         element.style.top = newTopPos + "px";
         element.style.left = newLeftPos + "px";
